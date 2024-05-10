425f1ae76468c3c4bbe8b9ec4af626de762f7e0c
Abhijeet Singh <116766794+iamabhijeet2003@users.noreply.github.com>
2024-05-10 16:57:56 +0000

    import pinia and css

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import '../src/assets/main.css'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+const app = createApp(App)
+
+
+app.use(createPinia())
+app.mount('#app')
