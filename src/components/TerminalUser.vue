19fa2eb541c3a59090c909e077f54931b4554c93
Abhijeet Singh <116766794+iamabhijeet2003@users.noreply.github.com>
2024-05-13 09:03:22 +0000

    add terminal user

---
+++ src/components/TerminalUser.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useTerminalStore } from '../store/TerminalStore'
+
+const { username } = storeToRefs(useTerminalStore())
+</script>
+
+<template>
+  <div class="flex mr-2.5 font-semibold">
+    <div class="text-secondary">{{ username }}</div>
+    @
+    <div class="text-primary">iamabhijeet2003</div>
+    :~$
+  </div>
+</template>
