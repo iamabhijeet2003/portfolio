c839859ac915324a702d375b75be583345bef130
Abhijeet Singh <116766794+iamabhijeet2003@users.noreply.github.com>
2024-05-14 16:26:07 +0000

    install tw-colors and create theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const { createThemes } = require('tw-colors')
+const themes = require('./src/utils/themes')
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'fira-code': ['Fira Code', 'monospace'],
+        'roboto-mono': ['Roboto Mono', 'monospace']
+      }
+    }
   },
-  plugins: [],
+  plugins: [createThemes(themes)]
 }
 
