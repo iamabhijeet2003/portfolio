1e68b624e489dbb5638879655be49dbc8a2389e4
Abhijeet Singh <116766794+iamabhijeet2003@users.noreply.github.com>
2024-05-16 16:42:51 +0000

    fix errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
   resolve: {
-    alias: [
+    alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    ],
-  },
+    }
+  }
 })
